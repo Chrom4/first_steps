a531ff4f7ea6274e6c479b95d704fc908d690594
Diogo Costa de Jesus <120536692+Chrom4@users.noreply.github.com>
2023-11-22 14:22:16 -0300

    idade.py

--- idade.py
+++ idade.py
@@ -1,2 +1,3 @@
+#Recebe uma entrada em dias e retorna quantos anos, meses e dias N corresponde
 N=int(input())
 print(f'{N//365} ano(s)\n{(400//30)-(400//365)*12} mes(es)\n{400%365-((400//30)-(400//365)*12)*30} dia(s)')
