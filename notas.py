b5f3aca6e52fb2fc58beef6c974a48b926a7b898
Diogo Costa de Jesus <120536692+Chrom4@users.noreply.github.com>
2023-11-22 14:23:37 -0300

    notas.py

--- notas.py
+++ notas.py
@@ -1,3 +1,4 @@
+#Recebe um valor em dinheiro (12,00) e retorna o número mínimo de cada nota que precisa para chegar nesse valor (1 de 10 reais e 1 de 2 reais)
 n=int(input())
 notas=[100,50,20,10,5,2,1]
 print(n)
